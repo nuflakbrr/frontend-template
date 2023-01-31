900f49b35e3485dfbf9b98e0777410c9a76b279c
Naufal Akbar <kbrnugroho@gmail.com>
2023-01-31 22:43:02 +0700

    upd: implement new rules

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,6 @@
-import ContainerHome from "../components/Containers/Home/Home"
+import React from 'react';
+
+import ContainerHome from '../components/Containers/Home/Home';
 
 export default function Home() {
   return <ContainerHome />;
