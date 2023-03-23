9b80fc18e6e41ffe89763c8ea53e29449620e86a
Naufal Akbar <kbrnugroho@gmail.com>
2023-03-24 02:10:39 +0700

    upd: change default link

--- src/data/headerNavLinks.ts
+++ src/data/headerNavLinks.ts
@@ -1,6 +1,5 @@
 export const headerNavLinks = [
   { title: 'Home', path: '/' },
   { title: 'About', path: '/about' },
-  { title: 'Facilities', path: '/facilities' },
-  { title: 'Contact Us', path: '/contact' },
+  { title: 'Contact', path: '/contact' },
 ];
