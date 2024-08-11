b47d5615fa6d4432e9fbb4df8b479dea660cb6e0
Naufal Akbar <naufalakbar378@gmail.com>
2024-08-11 14:29:45 +0700

    upd: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
+
 import ThemeProvider from '@/providers/ThemeProvider';
 
 const fontSans = FontSans({ subsets: ['latin'] });
